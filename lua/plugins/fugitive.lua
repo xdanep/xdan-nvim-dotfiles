fe6a013fd456ddc89e4791a43e85c16f9be9a725
xdanep <anielcb2002@gmail.com>
2024-03-04 21:34:10 -0600

    keymaps added to fugitive

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,4 +1,9 @@
 return {
 	"tpope/vim-fugitive",
-	
+	cmd = {"G", "Git"},
+	keys = {
+		{"<leader>ga", ":Git fetch --all -p<cr>", desc = "Git fetch"},
+		{"<leader>gl", ":Git pull<cr>", desc = "Git pull"},
+
+	},
 }

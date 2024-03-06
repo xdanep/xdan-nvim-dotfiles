36d9b57d336c4844066b74fcf16caca49dbf80bf
xdanep <anielcb2002@proton.me>
2024-03-05 21:43:32 -0600

    treesitter updated

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -1,5 +1,8 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
 	build = ":TSUpdate",
 	event = "VeryLazy",
 	main = "nvim-treesitter.configs",
@@ -17,6 +20,20 @@ return {
 		},
 		indent = {
 			enable = true,
+		},
+		textobjects = {
+			select = {
+				enable = true,
+				lookahead = true,
+				keymaps = {
+					["af"] = "@function.outer",
+					["if"] = "@function.inner",
+					["ac"] = "@conditional.outer",
+					["ic"] = "@conditional.inner",
+					["al"] = "@loop.outer",
+					["il"] = "@loop.inner",
+				}
+			},
 		},
 	},
 }

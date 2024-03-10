6df46367634836f995bc36a9c29a80d1284b3130
xdanep <anielcb2002@proton.me>
2024-03-09 23:52:06 -0600

    Bufferline problems solved

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,3 +1,4 @@
 vim.o.number = true
 vim.g.mapleader = " "
 vim.o.cursorline = true
+vim.opt.termguicolors = true
--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,17 +1,20 @@
 return {
-  "akinsho/bufferline.nvim",
+  'akinsho/bufferline.nvim',
   version = "*",
-  dependencies = "nvim-tree/nvim-web-devicons",
-  
-  require("bufferline").setup{
-    options = {
-      mode = 'buffers',
-      offsets = {
-        filetype = "NvimTree",
-        text = "File Explorer",
-        highlight = "Directory",
-        separator = true,
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  config = function()
+    require("bufferline").setup{
+      options = {
+        mode = "buffers",
+        offsets = {
+          {
+            filetype = "NvimTree",
+            text = "File Explorer",
+            text_align = "center",
+            separator = true
+          }
+        },
       },
-    },
-  },
+    }
+  end
 }

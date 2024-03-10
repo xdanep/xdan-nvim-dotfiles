e48cc23e56b8a19f677e9e10eb7e03df13b71f1b
xdanep <anielcb2002@proton.me>
2024-03-10 12:24:46 -0600

    Added file save mapping

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -5,5 +5,6 @@ map("n", "<C-l>", "<C-w>l", { desc = "Switch Window right" })
 map("n", "<C-j>", "<C-w>j", { desc = "Switch Window down" })
 map("n", "<C-k>", "<C-w>k", { desc = "Switch Window up" })
 
+map("n", "<C-s>", "<cmd>w<CR>", { desc = "File Save" })
 -- nvimtree
 map("n", "<C-b>", ":NvimTreeToggle<CR>", { desc = "Nvimtree Toggle window" })

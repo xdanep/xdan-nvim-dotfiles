c3a5a6af8b4b2e8e101978befea36be431bb333e
xdanep <anielcb2002@proton.me>
2024-06-30 15:19:24 -0600

    lsp_signature configuration modified

--- lua/plugins/lsp_signature.lua
+++ lua/plugins/lsp_signature.lua
@@ -1,6 +1,10 @@
 return {
   "ray-x/lsp_signature.nvim",
   event = "VeryLazy",
-  opts = {},
-  config = function(_, opts) require'lsp_signature'.setup(opts) end 
+  opts = {
+    handler_opts = {
+      border = "none"   -- double, rounded, single, shadow, none, or a table of borders
+    },
+  },
+  config = function(_, opts) require'lsp_signature'.setup(opts) end
 }
